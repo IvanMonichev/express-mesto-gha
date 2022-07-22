db5e58aab8aed42b38f8fe6731d6f65815a844bf
Ivan Monichev <63107880+IvanMonichev@users.noreply.github.com>
2022-07-22 16:40:42 +0300

    feat: add email and password to user schema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userScheme = new mongoose.Schema(
   {
@@ -17,6 +18,37 @@ const userScheme = new mongoose.Schema(
     avatar: {
       type: String,
       required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => {
+          validator.isEmail(email);
+        },
+        message: 'Формат записи поля E-Mail некорректен',
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (password) => {
+          validator.isStrongPassword(password, {
+            minLength: 6,
+            minLowercase: 1,
+            minUppercase: 1,
+            minNumbers: 1,
+            minSymbols: 0
+          })
+        },
+        message: "Формат записи поля Password некорректен. " +
+          "Пароль должен состоять минимум из 6 символов, " +
+          "1 символа нижнего регистра, " +
+          "1 символа верхнего регистра и содержать минимум 1 число",
+      },
     },
   },
   {
