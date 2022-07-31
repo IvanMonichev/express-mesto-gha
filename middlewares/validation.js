31aead362b1ffc712934126fbad673e4056e7068
Ivan Monichev <ivan@monichev.ru>
2022-07-31 14:01:45 +0300

    style: refactoring

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,11 +3,11 @@ const { linkRegularExpression, passwordRegularExpression } = require('../utils/r
 
 const registerValid = celebrate({
   body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(6).regex(passwordRegularExpression),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().regex(linkRegularExpression),
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(6).regex(passwordRegularExpression),
   }),
 });
 
