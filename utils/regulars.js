6096c7dc0d3de9c219721da56854ee0e42f9817d
Ivan Monichev <ivan@monichev.ru>
2022-07-31 20:49:34 +0300

    feat: change regular

--- utils/regulars.js
+++ utils/regulars.js
@@ -1,4 +1,4 @@
-const linkRegularExpression = /(http(s)?):\/\/(www.)?[a-z-\d]{1,256}.[a-z]{2,6}\b(\/[a-z]*)*/gm;
+const linkRegularExpression = /((http(s)?):\/\/(www.)?)?[a-z-\d]{1,256}.[a-z]{2,6}\b(\/[a-z]*)*/gm;
 
 module.exports = {
   linkRegularExpression,
