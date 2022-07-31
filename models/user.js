a6f3e44077f1457c437035e973d6fb55b3f0f276
Ivan Monichev <ivan@monichev.ru>
2022-07-31 14:28:45 +0300

    style: remove required avatar

--- models/user.js
+++ models/user.js
@@ -19,7 +19,6 @@ const userScheme = new mongoose.Schema(
     },
     avatar: {
       type: String,
-      required: true,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
     },
     email: {
